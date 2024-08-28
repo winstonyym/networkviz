6358c6d956dd5263a202d6d91bd20782eb027ccf
winstonyym <winstonyym@u.nus.edu>
2024-08-28 17:35:44 -0400

    add initial setup files

---
+++ readme.md
@@ -0,0 +1,13 @@
+### 1. Open terminal and navigate to preferred location
+cd <target_location>
+
+### 2. Git clone
+git clone <repo_url>
+
+### 3. Install environment
+conda env create -f environment.yml
+
+### 4. Activate environment
+conda activate networkviz
+
+### 5. ./notebooks/open json_and_csv.ipynb
---
+++ utils/utils.py
@@ -0,0 +1,35 @@
+# Map class utility functions
+import h5py
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from shapely import wkt
+
+
+def save_to_h5(filepath, gdf_dict, array_dict):
+
+    # Save to HDF5 file
+    with h5py.File(filepath, 'w') as f:
+
+        for gdf_key, gdf_data in gdf_dict.items():
+            # Save geodataframes as tables (convert to CSV or similar format for storage)
+            f.create_dataset(gdf_key, data=gdf_data.to_csv(index=False).encode())
+
+        for array_key, array_data in array_dict.items():
+            f.create_dataset(array_key, data=array_data)
+
+def load_from_h5(filepath):
+    connections = {}
+    objects = {}
+    with h5py.File(filepath, 'r') as f:
+
+        for k in f.keys():
+            if 'edges' not in k:
+                df = pd.read_csv(io.StringIO(f[k][()].decode()), sep=",")
+                gdf = gpd.GeoDataFrame(data=df, crs = 'EPSG:4326', geometry=df['geometry'].apply(wkt.loads))
+                objects[k] = gdf
+            else:
+                connections[k] = f[k][:]
+
+    return objects, connections
+        
